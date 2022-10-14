05a6d7c0a3e44d08b56c1d56777fde4d452b28e5
TimFoundATerminal <tom.timmons@outlook.com>
2022-10-14 22:29:36 +0100

    Adding transition functions and accepting states

--- src/DFA.java
+++ src/DFA.java
@@ -1,15 +1,44 @@
-
+import java.util.Objects;
 
 public class DFA {
 
-    private String[] states;
-    private char[] alphabet;
-    private String initialState;
-    private String[] acceptedStates;
+    private boolean valid = false;
+    final private String[] states;
+    final private String[] alphabet;
+    final private String initialState;
+    final private String[] acceptedStates;
+    final private String[][] transitions; // ["currentState", "arrow", "afterState"]
 
 
+    public DFA(String[] states, char[] alphabet, String initialState, String[] acceptedStates, String[][] transitions) {
+        this.states = states;
+        this.alphabet = alphabet;
+        this.initialState = initialState;
+        this.acceptedStates = acceptedStates;
+        this.transitions = transitions;
+        if (this.checkDFAValid()) {
+            this.valid = true;
+        }
+    }
 
-    public static void main(String[] args) {
+    private boolean checkDFAValid() {
+        return true;
+    }
 
+    private String[] getTransition(String currentState, String input) {
+        for (String[] transition : transitions) {
+            if (transition[0].equals(currentState) && transition[1].equals(input)) {
+                return transition;
+            }
+        }
+        return null;
+    }
+
+    public boolean isWord(String word) {
+        String currentState = this.initialState;
+        for (int i = 0; i < word.length(); i++) {
+
+        }
+        return false;
     }
 }
